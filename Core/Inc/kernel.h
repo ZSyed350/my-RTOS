7b59a8b4a35045c48542bfbd2a31df663f0a9816
zara <zsyed350@gmail.com>
2023-11-26 17:59:46 -0500

    osSched

--- Core/Inc/kernel.h
+++ Core/Inc/kernel.h
@@ -6,7 +6,10 @@
  */
 
 #ifndef INC_KERNEL_H_
-#define INC_KERNEL_H_
+#define INC_KERNEL_H
+#define SHPR2 *(uint32_t*)0xE000ED1C //for setting SVC priority, bits 31-24
+#define SHPR3 *(uint32_t*)0xE000ED20 // PendSV is bits 23-16
+#define _ICSR *(uint32_t*)0xE000ED04 //This lets us trigger PendSV_
 #include <stdbool.h>
 
 typedef struct k_thread{
--- Core/Src/kernel.c
+++ Core/Src/kernel.c
@@ -23,6 +23,9 @@ int curThreadIndx;
 
 extern void osKernelInitialize() {
 	MSP_INIT_VAL = *(uint32_t**)0x0;
+	//set the priority of PendSV to almost the weakest
+	SHPR3 |= 0xFE << 16; //shift the constant 0xFE 16 bits to set PendSV priority
+	SHPR2 |= 0xFDU << 24; //Set the priority of SVC higher than PendSV
 	current_stack = MSP_INIT_VAL;
 	curThreadIndx = 0;
 }
@@ -71,4 +74,11 @@ void SVC_Handler_Main( unsigned int *svc_args )
 	default: /* unknown SVC */
 		break;
 	}
+}
+
+void osSched()
+{
+	threads[curThreadIndx].sp = (uint32_t*)(__get_PSP() - 8*4);
+	curThreadIndx = (curThreadIndx+1)%nThreadsRunning;
+	__set_PSP((uint32_t)threads[curThreadIndx].sp);
 }
